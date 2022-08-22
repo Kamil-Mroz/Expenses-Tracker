9e8f932c03a7c92c635b8a395ae5457e9931e7c5
Kamil Mróz <kamilmp17@gmail.com>
2022-08-22 13:46:17 +0200

    refactroing and added sort logic

--- main.js
+++ main.js
@@ -3,20 +3,22 @@
 const btnAdd = document.querySelector(".add");
 const form = document.querySelector(".form");
 const tbodyEl = document.querySelector(".tbody");
+
 const inputDate = document.getElementById("date");
 const inputDescription = document.getElementById("description");
 const inputAmount = document.getElementById("amount");
 const inputSort = document.getElementById("sort");
 
 class expense {
-  #expense = [];
+  #expense = {};
   #id = 0;
 
   constructor() {
     this._getLocalStorage();
 
     form.addEventListener("submit", this.newExpense.bind(this));
-    tbodyEl.addEventListener("click", this.deleteEl.bind(this));
+    tbodyEl.addEventListener("click", this._deleteEl.bind(this));
+    inputSort.addEventListener("change", this._sort.bind(this));
   }
 
   _clearInputs() {
@@ -31,21 +33,60 @@ class expense {
     const description = inputDescription.value;
     const amount = +inputAmount.value;
 
-    // if (!dateValue || !description || amount < 0) return;
+    if (!dateValue || !description || amount < 0) return;
 
-    data = {
+    this.#expense[this.#id] = {
       id: this.#id,
       date: dateValue,
       description: description,
       amount: amount,
     };
+    console.log(this.#expense);
 
-    this.#expense.push(data);
     this._clearInputs();
+    this._renderExpenses(this.#expense[this.#id]);
     this.#id += 1;
-    this._renderExpenses(data);
 
     this._setLocalStorage();
+  }
+
+  _sort() {
+    let sorted = Object.values(this.#expense);
+    if (!sorted || sorted.length === 0) return;
+    if (!inputSort.value) return;
+    const method = inputSort.value;
+    const sorting = {
+      dateUp: function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+      },
+      dateDown: function (a, b) {
+        return new Date(a.date) - new Date(b.date);
+      },
+      amountUp: function (a, b) {
+        return b.amount - a.amount;
+      },
+      amountDown: function (a, b) {
+        return a.amount - b.amount;
+      },
+      DescriptionUp: function (a, b) {
+        return b.description.localeCompare(a.description, "en", {
+          sensitivity: "base",
+        });
+      },
+      DescriptionDown: function (a, b) {
+        return a.description.localeCompare(b.description, "en", {
+          sensitivity: "base",
+        });
+      },
+    };
+
+    sorted.sort(sorting[method]);
+
+    tbodyEl.innerHTML = "";
+    sorted.forEach((el) => {
+      this._renderExpenses(el);
+    });
+    sorted = Object.values(this.#expense);
   }
 
   _renderExpenses(data) {
@@ -61,16 +102,16 @@ class expense {
     tbodyEl.insertAdjacentHTML("afterbegin", expense);
   }
 
-  deleteEl(e) {
+  _deleteEl(e) {
     e.preventDefault();
 
     const expenseEl = e.target.closest(".delete");
     if (!expenseEl) return;
     const expensesRow = expenseEl.closest("tr");
-    const id = +expensesRow.dataset["id"];
+    const id = expensesRow.dataset["id"];
+    console.log(id);
     expensesRow.remove();
-
-    this.#expense.splice(id, 1);
+    delete this.#expense[id];
 
     this._setLocalStorage();
   }
@@ -82,21 +123,14 @@ class expense {
   _getLocalStorage() {
     const data = JSON.parse(localStorage.getItem("expense"));
 
-    if (!data || data.length === 0) return;
+    if (!data || Object.keys(data).length === 0) return;
     this.#expense = data;
-    this.#id = this.#expense.at(-1).id + 1;
 
-    this.#expense.forEach((expense) => {
-      this._renderExpenses(expense);
-    });
-  }
+    this.#id = +Object.keys(this.#expense).at(-1) + 1;
 
-  _displayTotal() {
-    const sum = this.#expense.reduce((sum, curr) => {
-      sum += curr.amount;
-      return sum;
-    }, 0);
-    console.log(sum);
+    for (const el of Object.values(this.#expense)) {
+      this._renderExpenses(el);
+    }
   }
 }
 
